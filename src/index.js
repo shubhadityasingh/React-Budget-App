c38d02ae0cd10da36aa1445a0d3181f5fb6cd1ae
Shubhaditya <shubh28062000@gmail.com>
2022-01-23 20:52:46 +0530

    1st commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetsProvider } from './contexts/BudgetsContext'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BudgetsProvider>
     <App />
+    </BudgetsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
